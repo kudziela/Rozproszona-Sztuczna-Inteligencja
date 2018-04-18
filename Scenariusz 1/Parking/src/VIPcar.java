70eb735f5780692fa862b46077f561bedc29d2f3
kudziela <32766028+kudziela@users.noreply.github.com>
2018-04-18 22:37:35 +0200

    Add files via upload

---
+++ Scenariusz 1/Parking/src/VIPcar.java
@@ -0,0 +1,8 @@
+public class VIPcar extends Car{
+
+    public VIPcar(String name, String model, String number){
+        this.name = name;
+        this.model = model;
+        this.number = number;
+    }
+}
---
+++ Scenariusz 1/Parking/src/normalCar.java
@@ -0,0 +1,22 @@
+public class normalCar extends Car{
+
+    normalCar[] cars;
+    int numberOfCars;
+
+    public normalCar(String name, String model, String number){
+        this.name = name;
+        this.model = model;
+        this.number = number;
+    }
+
+    public normalCar(){
+    }
+
+    public void makeCarsTable() {
+        for (int i = 0; i < numberOfCars; i++) {
+            cars[i] = null;
+        }
+
+
+    }
+}
